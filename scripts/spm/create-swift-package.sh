24a4969b6cfbc85a4e3b39804260e2f5bae1dcc0
ykkd <ykkd221dev@gmail.com>
2024-10-12 20:30:22 +0900

    fix: typo

--- scripts/spm/create-swift-package.sh
+++ scripts/spm/create-swift-package.sh
@@ -3,7 +3,7 @@
 echo "Enter package name:"
 read package_name
 
-mkdir -p "Pacakges/$package_name"
+mkdir -p "Packages/$package_name"
 
-cd "Pacakges/$package_name" && \
+cd "Packages/$package_name" && \
 swift package init --type library --enable-swift-testing --name $package_name
