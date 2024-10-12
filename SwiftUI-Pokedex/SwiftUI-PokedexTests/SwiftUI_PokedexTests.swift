07fe225603e95e4ce58161c6d0325d5ec205bf93
ykkd <ykkd221dev@gmail.com>
2024-10-12 18:06:20 +0900

    refactor: remove unneeded test codes

--- SwiftUI-Pokedex/SwiftUI-PokedexTests/SwiftUI_PokedexTests.swift
+++ SwiftUI-Pokedex/SwiftUI-PokedexTests/SwiftUI_PokedexTests.swift
@@ -8,10 +8,5 @@
 import Testing
 
 struct SwiftUI_PokedexTests {
-
-    @Test
-    func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+    // NOP
 }
