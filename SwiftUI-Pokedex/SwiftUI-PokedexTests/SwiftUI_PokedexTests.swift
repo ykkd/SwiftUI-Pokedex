b427effe2fa44694bbb9281900dd45d5ba62ba02
ykkd <ykkd221dev@gmail.com>
2024-10-12 17:58:29 +0900

    feat: initial setup

---
+++ SwiftUI-Pokedex/SwiftUI-PokedexTests/SwiftUI_PokedexTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PokedexTests.swift
+//  SwiftUI-PokedexTests
+//
+//  Created by ykkd on 2024/10/12.
+//
+
+import Testing
+
+struct SwiftUI_PokedexTests {
+
+    @Test
+    func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
