010167443150b7deb19532edf56b8be8253600a3
ykkd <ykkd221dev@gmail.com>
2024-10-14 14:05:34 +0900

    fix: add `withNavigation` in `RouterView`

--- Packages/AppPackage/Sources/Router/Router/RouterView.swift
+++ Packages/AppPackage/Sources/Router/Router/RouterView.swift
@@ -14,26 +14,44 @@ public struct RouterView<Content: View>: View {
 
     @StateObject private var router: Router
 
+    private let withNavigation: Bool
+
     private let content: Content
 
     public init(
         router: Router,
+        withNavigation: Bool,
         @ViewBuilder content: @escaping () -> Content
     ) {
         _router = StateObject(wrappedValue: router)
+        self.withNavigation = withNavigation
         self.content = content()
     }
 
     public var body: some View {
-        NavigationStack(path: router.navigationPath) {
+        content(withNavigation)
+            .sheet(item: router.presentingSheet) { screen in
+                router.view(screen, transition: .sheet)
+            }
+            .fullScreenCover(item: router.presentingFullScreen) { screen in
+                router.view(screen, transition: .fullScreen)
+            }
+    }
+
+    @ViewBuilder
+    private func content(_ withNavigation: Bool) -> some View {
+        if withNavigation {
+            NavigationStack(path: router.navigationPath) {
+                content
+                    .navigationDestination(for: Screen.self) { screen in
+                        router.view(screen, transition: .push)
+                    }
+            }
+        } else {
             content
                 .navigationDestination(for: Screen.self) { screen in
                     router.view(screen, transition: .push)
                 }
-        }.sheet(item: router.presentingSheet) { screen in
-            router.view(screen, transition: .sheet)
-        }.fullScreenCover(item: router.presentingFullScreen) { screen in
-            router.view(screen, transition: .fullScreen)
         }
     }
 }
