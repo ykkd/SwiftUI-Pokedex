1d6925cba01b65ed374cbb2c4f334a68735da46d
ykkd <ykkd221dev@gmail.com>
2024-10-17 21:37:14 +0900

    feat: implement view basics

--- Packages/AppPackage/Sources/Screens/PokemonDetailScreen/PokemonDetailView.swift
+++ Packages/AppPackage/Sources/Screens/PokemonDetailScreen/PokemonDetailView.swift
@@ -5,4 +5,66 @@
 //  Created by ykkd on 2024/10/17.
 //
 
-import Foundation
+private import Dependencies
+private import DependencyContainer
+import Entity
+import Router
+import SwiftUI
+private import DesignSystem
+private import SFSafeSymbols
+private import ScreenExtension
+
+// MARK: - PokemonDetailView
+public struct PokemonDetailView: View {
+
+    @StateObject private var router: Router
+
+    @State private var state: PokemonDetailViewState
+
+    private let input: CommonScreenInput
+
+    public init(
+        router: Router,
+        input: CommonScreenInput,
+        pokemon: Pokemon
+    ) {
+        _router = StateObject(wrappedValue: router)
+        self.input = input
+        state = .init(pokemon: pokemon)
+    }
+
+    public var body: some View {
+        RouterView(
+            router: router,
+            withNavigation: input.withNavigation
+        ) {
+            content()
+                .when(true) { _ in
+                    emptyView()
+                }
+        }
+    }
+}
+
+extension PokemonDetailView {
+
+    private func content() -> some View {
+        EmptyView()
+    }
+}
+
+extension PokemonDetailView {
+
+    private func emptyView() -> some View {
+        GeometryReader { geometry in
+            CenteringView {
+                ProgressView()
+                    .frame(width: 64, height: 64)
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .refreshableScrollView(spaceName: "PokemonDetailEmptyState") {
+                // TODO: implement
+            }
+        }
+    }
+}
--- Packages/AppPackage/Sources/Screens/PokemonDetailScreen/PokemonDetailViewState.swift
+++ Packages/AppPackage/Sources/Screens/PokemonDetailScreen/PokemonDetailViewState.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Observation
+private import Dependencies
+import Entity
+import Logger
+
+// MARK: - PokemonDetailViewState
+@MainActor
+@Observable
+final class PokemonDetailViewState {
+
+    let pokemon: Pokemon
+
+    @ObservationIgnored
+    @Dependency(\.mainLogger) private var logger
+
+    private(set) var isLoading: Bool = false {
+        didSet {
+            logger.log(.debug, message: "isLoading: \(isLoading)")
+        }
+    }
+
+    init(pokemon: Pokemon) {
+        self.pokemon = pokemon
+    }
+}
