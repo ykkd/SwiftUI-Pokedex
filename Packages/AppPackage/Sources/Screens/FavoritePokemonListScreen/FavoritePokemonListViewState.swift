f1911392bd99f4ffc7140a89671aa691ca5d8c92
ykkd <ykkd221dev@gmail.com>
2024-10-19 02:03:37 +0900

    feat: implement `FavoritePokemonListViewState` base

--- Packages/AppPackage/Sources/Screens/FavoritePokemonListScreen/FavoritePokemonListViewState.swift
+++ Packages/AppPackage/Sources/Screens/FavoritePokemonListScreen/FavoritePokemonListViewState.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import GetPokemonListUseCase
+import Observation
+private import Dependencies
+import Entity
+import Logger
+
+// MARK: - FavoritePokemonListViewState
+@MainActor
+@Observable
+final class FavoritePokemonListViewState {
+
+    @ObservationIgnored
+    @Dependency(\.getPokemonListUseCase) private var getPokemonListUseCase
+
+    @ObservationIgnored
+    @Dependency(\.mainLogger) private var logger
+
+    private(set) var pokemons: [Pokemon] = [] {
+        didSet {
+            logger.log(.debug, message: "number of pokemons: \(pokemons.count)")
+        }
+    }
+
+    private(set) var isLoading: Bool = false {
+        didSet {
+            logger.log(.debug, message: "isLoading: \(isLoading)")
+        }
+    }
+
+    var shouldShowEmptyView: Bool {
+        pokemons.isEmpty
+    }
+
+    init() {}
+
+    func getData() async {}
+
+    func refresh() async {}
+}
