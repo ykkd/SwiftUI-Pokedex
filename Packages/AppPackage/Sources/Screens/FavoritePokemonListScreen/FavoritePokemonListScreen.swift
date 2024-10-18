4309eaa88b52ab79089bf3ff3724f5c84abe4a65
ykkd <ykkd221dev@gmail.com>
2024-10-19 01:56:36 +0900

    feat: add `FavoritePokemonListScreen` target

---
+++ Packages/AppPackage/Sources/Screens/FavoritePokemonListScreen/FavoritePokemonListScreen.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritePokemonListScreen.swift
+//  AppPackage
+//
+//  Created by ykkd on 2024/10/19.
+//
+
+import Foundation
---
+++ Packages/AppPackage/Sources/Screens/FavoritePokemonListScreen/FavoritePokemonListViewState.swift
@@ -0,0 +1,8 @@
+//
+//  FavoritePokemonListViewState.swift
+//  AppPackage
+//
+//  Created by ykkd on 2024/10/19.
+//
+
+import Foundation
